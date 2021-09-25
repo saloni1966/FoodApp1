c32aade8354db9d9aa0de0781dbe073b16cea7f7
Saloni Sharma <68701930+saloni1966@users.noreply.github.com>
2021-09-25 06:41:30 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDMK7i2C4cBidEPBvB_UbCoCVf3m12Rz_4",
+  authDomain: "storytelling-k.firebaseapp.com",
+  databaseURL: "https://storytelling-k-default-rtdb.firebaseio.com",
+  projectId: "storytelling-k",
+  storageBucket: "storytelling-k.appspot.com",
+  messagingSenderId: "299649739382",
+  appId: "1:299649739382:web:e74ec8c0c2f881452ecd57"
+};
